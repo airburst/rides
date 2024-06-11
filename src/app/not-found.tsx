40915938915c3c9a11bc545af5f36fd73456510c
Mark Fairhurst <mark@fairhursts.net>
2024-06-11 14:53:22 +0100

    feat: Add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import { BackButton, CircleQuestionIcon, MainContent } from "@/components";
+
+const NotFound = () => (
+  <MainContent>
+    <div className=" text-neutral-800">
+      <div className="flex h-64 items-center justify-center text-8xl text-sky-500">
+        <CircleQuestionIcon className="fill-teal-500 w-24 h-24" />
+      </div>
+      <div className="flex items-center p-4 text-center text-2xl text-neutral-700">
+        Sorry - we can&apos;t find this page.
+      </div>
+      <div className="flex items-center justify-center p-4  text-neutral-700">
+        <BackButton />
+      </div>
+    </div>
+  </MainContent>
+);
+
+export default NotFound;
