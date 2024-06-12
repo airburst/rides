2e6d2b54359928356975dcbe051870970f3598b4
Mark Fairhurst <mark@fairhursts.net>
2024-06-12 19:33:36 +0100

    chore: Remove comments

--- src/server/actions/updateProfile.ts
+++ src/server/actions/updateProfile.ts
@@ -4,19 +4,14 @@ import { type Preferences } from "@/types";
 import { updateUser } from "./updateUser";
 
 export const updateProfile = async (formData: FormData) => {
-  console.log("🚀 ~ updateProfile ~ formData:", formData);
-  ("use server");
   const id = formData.get("id") as string;
-  console.log("🚀 ~ updateProfile ~ id:", id);
   const name = formData.get("name") as string;
-  console.log("🚀 ~ updateProfile ~ name:", name);
   const mobile = formData.get("mobile") as string;
   const emergency = formData.get("emergency") as string;
   const units = formData.get("units") as string;
   const preferences = { units } as Preferences;
 
   const result = await updateUser({ id, name, mobile, emergency, preferences });
-  console.log("🚀 ~ formAction ~ result:", result);
 
   return result;
 };
