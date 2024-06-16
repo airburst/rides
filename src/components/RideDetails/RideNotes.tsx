7ea20c33607dbef6f08d93511ada985a2807bf0c
Mark Fairhurst <mark@fairhursts.net>
2024-06-16 16:06:17 +0100

    feat: Use latest version of Dialog

--- src/components/RideDetails/RideNotes.tsx
+++ src/components/RideDetails/RideNotes.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { updateRideNotes } from "@/server/actions/updateRideNote";
-import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { useState } from "react";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { RideNotesForm, type FormValues } from "../forms/RideNotesForm";
 
@@ -44,58 +44,22 @@ export const RideNotes = ({
   };
 
   return (
-    <Transition appear show={showNotesForm} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={closeHandler}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
-                >
-                  Add Message
-                </Dialog.Title>
-
-                <div className="mt-2">
-                  <div className="w-full text-neutral-800">
-                    <RideNotesForm
-                      defaultValues={defaultValues}
-                      errors={errors}
-                      isDirty={isDirty}
-                      register={register}
-                      setValue={setValue}
-                      handleSubmit={handleSubmit(onSubmit)}
-                      waiting={waiting}
-                      closeHandler={closeHandler}
-                    />
-                  </div>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
+    <Dialog open={showNotesForm} onClose={closeHandler} className="relative z-10">
+      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+        <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
+          <DialogTitle className="font-bold">Add Message</DialogTitle>
+          <RideNotesForm
+            defaultValues={defaultValues}
+            errors={errors}
+            isDirty={isDirty}
+            register={register}
+            setValue={setValue}
+            handleSubmit={handleSubmit(onSubmit)}
+            waiting={waiting}
+            closeHandler={closeHandler}
+          />
+        </DialogPanel>
+      </div>
+    </Dialog>
   );
 };
