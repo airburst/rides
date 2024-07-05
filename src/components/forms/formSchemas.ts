2af88f1339abf378f7b7a96c06e2f068f8c165f8
Mark Fairhurst <mark@fairhursts.net>
2024-07-05 16:58:41 +0100

    fix: Enforce contact number

--- src/components/forms/formSchemas.ts
+++ src/components/forms/formSchemas.ts
@@ -2,14 +2,27 @@ import { z } from "zod";
 import { zfd } from "zod-form-data";
 
 //  User
+const phoneRegex = new RegExp(
+  /([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/,
+);
+
 export const userProfileFormSchema = z.object({
   id: z.string().trim().min(3, { message: "Id is required" }),
-  name: z.string().trim().min(3, { message: "Name is required" }),
-  mobile: z.string().trim().min(3, { message: "Mobile number is required" }),
-  emergency: z
-    .string()
+  name: z
+    .string({ required_error: "Name is required" })
     .trim()
-    .min(3, { message: "Emergency contact is required" }),
+    .min(3, { message: "Name must contain at least 3 letters" }),
+  mobile: z
+    .string({ required_error: "Mobile number is required" })
+    .trim()
+    .min(3, { message: "Mobile number is required" }),
+  emergency: z
+    .string({
+      required_error: "An emergency contact (with number) is required",
+    })
+    .trim()
+    .regex(phoneRegex, "Emergency contact must include a telephone number")
+    .min(11, { message: "Too short for an emergency contact and number" }),
   email: z.string().trim().email({ message: "Invalid email address" }),
   preferences: z.object({
     units: z.string(),
