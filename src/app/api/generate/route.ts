5de3b635633e98a4c028280c6b9f2b2113b94fcc
Mark Fairhurst <mark@fairhursts.net>
2024-07-08 20:15:56 +0100

    fix: Refactor functions out of generate route file

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,16 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { generateRides } from "@/server/actions/generate-rides";
-import { getRepeatingRide } from "@/server/actions/get-repeating-ride";
-import { getRepeatingRides } from "@/server/actions/get-repeating-rides";
-import { type RepeatingRideDb, type RideSet } from "@/types";
 import { getNextMonth } from "@utils/dates";
-import {
-  makeRidesInPeriod,
-  repeatingRideToDb,
-  updateRRuleStartDate,
-} from "@utils/repeatingRides";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { createRides, getRidesFromTemplates } from "./utils";
 
 /**
  * This API is designed to be hit by a scheduled workflow
@@ -18,11 +10,6 @@ import { NextResponse } from "next/server";
  * and the default is to generate all rides for the following month.
  * You can override that by passing a date in the POST body.
  */
-type BodyProps = {
-  scheduleId?: string;
-  date: string;
-};
-
 export type GenerateApiResponse = {
   success: boolean;
   generateFromDate?: string;
@@ -32,53 +19,6 @@ export type GenerateApiResponse = {
     count?: number;
     error?: string;
   }[];
-};
-
-const getRidesFromTemplates = async ({ scheduleId, date }: BodyProps) => {
-  // If we have an id, find one match, else return all templates
-  let templates: RepeatingRideDb[] = [];
-
-  if (scheduleId) {
-    const result = await getRepeatingRide(scheduleId);
-
-    if (result.ride) {
-      const rideWithSchedule = repeatingRideToDb(result.ride);
-
-      templates.push(rideWithSchedule);
-    }
-  } else {
-    const result = await getRepeatingRides();
-
-    templates = result.rides.map((ride) => repeatingRideToDb(ride));
-  }
-
-  // For each template, get a list rides to create
-  return templates.map((template) => makeRidesInPeriod(template, date));
-};
-
-export const createRides = async (rideSet: RideSet[]) => {
-  // Map over each array of rides
-  const resultPromises = rideSet.map(
-    async ({ id, rides, schedule }: RideSet) => {
-      if (rides.length === 0) {
-        return { scheduleId: id, count: 0 };
-      }
-      // find the latest date to update template
-      const lastDate = rides?.at(-1)?.rideDate;
-      const updatedSchedule = updateRRuleStartDate(schedule, lastDate);
-      const result = await generateRides(rides, id!, updatedSchedule);
-
-      if (!result.success) {
-        return { error: result.message };
-      }
-
-      return { scheduleId: id, count: result.createdRides };
-    },
-  );
-
-  const results = await Promise.all(resultPromises);
-
-  return results;
 };
 
 export async function POST(
---
+++ src/app/api/generate/utils.ts
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { generateRides } from "@/server/actions/generate-rides";
+import { getRepeatingRide } from "@/server/actions/get-repeating-ride";
+import { getRepeatingRides } from "@/server/actions/get-repeating-rides";
+import { type RepeatingRideDb, type RideSet } from "@/types";
+import {
+  makeRidesInPeriod,
+  repeatingRideToDb,
+  updateRRuleStartDate,
+} from "@utils/repeatingRides";
+
+type BodyProps = {
+  scheduleId?: string;
+  date: string;
+};
+
+export const getRidesFromTemplates = async ({
+  scheduleId,
+  date,
+}: BodyProps) => {
+  // If we have an id, find one match, else return all templates
+  let templates: RepeatingRideDb[] = [];
+
+  if (scheduleId) {
+    const result = await getRepeatingRide(scheduleId);
+
+    if (result.ride) {
+      const rideWithSchedule = repeatingRideToDb(result.ride);
+
+      templates.push(rideWithSchedule);
+    }
+  } else {
+    const result = await getRepeatingRides();
+
+    templates = result.rides.map((ride) => repeatingRideToDb(ride));
+  }
+
+  // For each template, get a list rides to create
+  return templates.map((template) => makeRidesInPeriod(template, date));
+};
+
+export const createRides = async (rideSet: RideSet[]) => {
+  // Map over each array of rides
+  const resultPromises = rideSet.map(
+    async ({ id, rides, schedule }: RideSet) => {
+      if (rides.length === 0) {
+        return { scheduleId: id, count: 0 };
+      }
+      // find the latest date to update template
+      const lastDate = rides?.at(-1)?.rideDate;
+      const updatedSchedule = updateRRuleStartDate(schedule, lastDate);
+      const result = await generateRides(rides, id!, updatedSchedule);
+
+      if (!result.success) {
+        return { error: result.message };
+      }
+
+      return { scheduleId: id, count: result.createdRides };
+    },
+  );
+
+  const results = await Promise.all(resultPromises);
+
+  return results;
+};
