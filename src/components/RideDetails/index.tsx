3303624675d85f1662ea9bebdc2ad46e73aad85e
Mark Fairhurst <mark@fairhursts.net>
2024-07-10 17:56:57 +0100

    fix: Note button

--- src/components/RideDetails/index.tsx
+++ src/components/RideDetails/index.tsx
@@ -82,18 +82,17 @@ export const RideDetails = ({ ride, user, role }: Props) => {
               isLeader={isLeader}
             />
             <div className="grid grid-cols-3 mb-16 md:gap-4 gap-2 p-2 sm:px-0 md:max-w-[460px]">
-              <BackButton className="md:min-w-32" url={`/#${id}`} />
+              <BackButton url={`/#${id}`} />
 
               {isGoing && (
                 <Button accent onClick={openNotes}>
                   <MessageSquare className="w-6 h-6" />
-                  MESSAGE
+                  NOTE
                 </Button>
               )}
 
               {user && (canJoin || isGoing) && (
                 <JoinButton
-                  className="md:min-w-32"
                   going={isGoing}
                   ariaLabel={`Join ${name} ride`}
                   rideId={id!}
