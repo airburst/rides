41237f5d59fbe33a110c012e0bb445d43f9cbca3
Mark Fairhurst <mark@fairhursts.net>
2024-12-10 08:31:09 +0000

    feat: Increase short text length

--- src/components/Markdown/Viewer.tsx
+++ src/components/Markdown/Viewer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { NOTES_SHOW_MORE_LENGTH } from "@/constants";
 import clsx from "clsx";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import markdownIt from "markdown-it";
@@ -12,35 +13,40 @@ export type ViewerProps = {
   title?: string;
 };
 
-const SHORT_TEXT_LENGTH = 300;
-
 const Viewer = ({ markdown, title }: ViewerProps) => {
   const [showAll, setShowAll] = useState(false);
 
   const toggleShowAll = () => {
     setShowAll(!showAll);
-  }
+  };
 
   const md = new markdownIt({
     html: true,
     linkify: true,
-    typographer: true
+    typographer: true,
   });
   const html = md.render(markdown ?? "");
   // Add a show more/less button if the text is long
-  const isLong = html.length > SHORT_TEXT_LENGTH;
-  const displayText = showAll ? html : `${html.slice(0, SHORT_TEXT_LENGTH)}${isLong ? "..." : ""}`;
+  const isLong = html.length > NOTES_SHOW_MORE_LENGTH;
+  const displayText = showAll
+    ? html
+    : `${html.slice(0, NOTES_SHOW_MORE_LENGTH)}${isLong ? "..." : ""}`;
   const notesClass = clsx("col-span-2", showAll ? "mb-4" : "mb-2");
-  const showMoreClass = "w-full h-[24px] flex justify-center absolute bottom-4 bg-gradient-to-t from-white";
+  const showMoreClass =
+    "w-full h-[24px] flex justify-center absolute bottom-4 bg-gradient-to-t from-white";
 
   return (
     <div className="flex w-full flex-col gap-2 rounded bg-white py-2 shadow-md">
-      {title && <div className="px-2 text-xl font-bold tracking-wide text-neutral-700">
-        {title}
-      </div>}
+      {title && (
+        <div className="px-2 text-xl font-bold tracking-wide text-neutral-700">
+          {title}
+        </div>
+      )}
 
-      <div className="relative grid w-full grid-cols-[100px_1fr] items-center justify-between px-2 font-normal md:grid-cols-[220px_1fr] md:justify-start md:gap-4 gap-2">
-        <div id="ride-notes" className={notesClass}
+      <div className="relative grid w-full grid-cols-[100px_1fr] items-center justify-between gap-2 px-2 font-normal md:grid-cols-[220px_1fr] md:justify-start md:gap-4">
+        <div
+          id="ride-notes"
+          className={notesClass}
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{ __html: displayText }}
         />
@@ -48,13 +54,17 @@ const Viewer = ({ markdown, title }: ViewerProps) => {
           <div className={showMoreClass}>
             <Button className="font-light" link onClick={toggleShowAll}>
               {showAll ? "Show less" : "Show more"}
-              {showAll ? <ChevronUp className="w-4 h-4" /> : <ChevronDown className="w-4 h-4" />}
+              {showAll ? (
+                <ChevronUp className="h-4 w-4" />
+              ) : (
+                <ChevronDown className="h-4 w-4" />
+              )}
             </Button>
           </div>
         )}
       </div>
     </div>
   );
-}
+};
 
 export default Viewer;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,3 +10,5 @@ export const RIDER_LIMIT_OPTIONS: number[] = Array.from(Array(45).keys()).map(
 export const DATABASE_PREFIX = "bcc_";
 
 export const MAX_FILE_SIZE_IN_BYTES = 4 * 1024 * 1024; // 4mb
+
+export const NOTES_SHOW_MORE_LENGTH = 400;
