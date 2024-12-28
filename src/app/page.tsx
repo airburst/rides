274060ff58f03a10d39bac738c7e0a6d5c169fe7
Mark Fairhurst <mark@fairhursts.net>
2024-12-28 13:36:09 +0000

    chore: Turn snowfall off

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { MainContent } from "@/components/Layout/MainContent";
 import { type RidesListProps } from "@/components/RidesList";
-import Snow from "@/components/Snow/Snow";
+// import Snow from "@/components/Snow/Snow";
 import { UnregisterServiceWorkers } from "@/components/UnregisterServiceWorkers";
 import { env } from "@/env";
 import type { Metadata } from "next";
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default async function HomePage() {
   return (
     <>
-      <Snow />
+      {/* <Snow /> */}
       <MainContent>
         <RidesList />
       </MainContent>
