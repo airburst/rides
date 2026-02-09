3a6ac86320c85c13bfd80f9ec946f5802acf746f
Mark Fairhurst <mark@fairhursts.net>
2026-02-09 20:08:53 +0000

    chore: Remove redundant prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
   plugins: ["prettier-plugin-tailwindcss", "@prettier/plugin-oxc"],
 };
 
